03dc438c404303fa38293d8924c24c062e3452fe
Sampath <nupa.sampath2@gmail.com>
2019-10-30 12:19:15 +0530

    home controller initialization

---
+++ src/main/java/com/testBoot/demo/HomeController.java
@@ -0,0 +1,12 @@
+package com.testBoot.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+
+@RestController  
+public class HomeController {  
+    @RequestMapping("/login")  
+    public String hello(){  
+        return"Returns Login Code Here";  
+    }  
+}  

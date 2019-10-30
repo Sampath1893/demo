1d067d123161542e0038a82865ee92f8e81986ac
Sampath <nupa.sampath2@gmail.com>
2019-10-30 12:16:31 +0530

    initial commit

---
+++ src/test/java/com/testBoot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testBoot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
